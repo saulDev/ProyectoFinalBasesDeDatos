816da5821a72aa777cd15a737a8a90c676895abb
Saul <saul@moobky.com>
2018-03-16 20:33:23 -0600

    ok!

---
+++ Final/Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Final
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btbLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(88, 99);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 0;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(88, 54);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btbLogin
+            // 
+            this.btbLogin.Location = new System.Drawing.Point(72, 167);
+            this.btbLogin.Name = "btbLogin";
+            this.btbLogin.Size = new System.Drawing.Size(129, 23);
+            this.btbLogin.TabIndex = 4;
+            this.btbLogin.Text = "Iiniciar";
+            this.btbLogin.UseVisualStyleBackColor = true;
+            this.btbLogin.Click += new System.EventHandler(this.btbLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btbLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPwd);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btbLogin;
+    }
+}
+
---
+++ Final/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class Login : Form
+    {
+        protected string userName = "rinku";
+        protected string passwrod = "123";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btbLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string password = txtPwd.Text;
+
+            bool authenticated = check_UserPassworMatch(user, password);
+
+            if (authenticated)
+            {
+                MessageBox.Show("Bienvenido.");
+            }
+            else
+                MessageBox.Show("Password o usuario incorrectos.");
+        }
+
+        private bool check_UserPassworMatch(string user, string password)
+        {
+            if (user == "rinku" && password == "123")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
